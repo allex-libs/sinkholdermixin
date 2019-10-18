dae2bf2ee56f6474cc1a8e1f5aa6cb9fe000f967
Andrija Petrovic <andrija.hers@gmail.com>
2019-10-18 11:21:12 +0000

    Improvements

--- index.js
+++ index.js
@@ -26,35 +26,59 @@ function createSinkHolderMixin (execlib) {
   }
 
   function SinkHolderMixin () {
+    this.sinkHolderMixinAlive = true;
     this.sink = null;
     this.sinkDestroyedListener = null;
   }
   SinkHolderMixin.prototype.destroy = function () {
     purgeSinkHolderListener(this);
     purgeSinkHolderSink(this);
+    this.sinkHolderMixinAlive = null;
   };
   SinkHolderMixin.prototype.registerSink = function (sink) {
+    if (!this.sinkHolderMixinAlive) {
+      if (sink && sink.destroyed) {
+        sink.destroy();
+      }
+      return;
+    }
     purgeSinkHolderListener(this);
     purgeSinkHolderSink(this);
     this.sink = sink;
-    if (sink) {
+    if (sink && sink.destroyed) {
       this.sinkDestroyedListener = sink.destroyed.attach(this.onSinkDown.bind(this));
+      return q(sink);
     }
-    return q(sink);
+    return this.activate();
   };
   SinkHolderMixin.prototype.onSinkDown = function (modulename) {
+    console.log(this.constructor.name, 'onSinkDown?');
+    console.trace();
+    if (!this.sinkHolderMixinAlive) {
+      return;
+    }
     this.sink = null;
     this.activate();
   };
   SinkHolderMixin.prototype.activate = function () {
+    var ret;
+    if (!this.sinkHolderMixinAlive) {
+      return q(false);
+    }
     if (this.sink) {
       return q(true);
     }
     //Mixin-ers have to implement acquireSink
-    return this.acquireSink();
+    ret = this.acquireSink();
+    ret.then(this.registerSink.bind(this));
+    return ret;
   };
   SinkHolderMixin.prototype.deactivate = function () {
-    var d = q.defer();
+    var d;
+    if (!this.sinkHolderMixinAlive) {
+      return q(false);
+    }
+    d = q.defer();
     if (!(this.sink && this.sink.destroyed)) {
       d.resolve(true);
     } else {
